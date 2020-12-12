3b5923c06232757953fe382456766881d19f748c
Bharath Dasaraju <bharathkumarraju_dasaraju@ntucenterprise.sg>
2020-12-12 14:08:36 +0800

    Code with python :

---
+++ 2.WebDevelopment/demo/hellopython_flask/1.sh
@@ -0,0 +1,24 @@
+FLASK_APP=service.py flask run
+
+bharathkumarraju@R77-NB193 hellopython_flask % FLASK_APP=service.py flask run
+ * Serving Flask app "service.py"
+ * Environment: production
+   WARNING: This is a development server. Do not use it in a production deployment.
+   Use a production WSGI server instead.
+ * Debug mode: off
+ * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+127.0.0.1 - - [05/Oct/2020 19:33:25] "GET / HTTP/1.1" 200 -
+127.0.0.1 - - [05/Oct/2020 19:33:25] "GET /favicon.ico HTTP/1.1" 404 -
+127.0.0.1 - - [05/Oct/2020 19:38:14] "GET / HTTP/1.1" 200 -
+127.0.0.1 - - [05/Oct/2020 19:38:20] "GET / HTTP/1.1" 200 -
+127.0.0.1 - - [05/Oct/2020 19:38:20] "GET / HTTP/1.1" 200 -
+127.0.0.1 - - [05/Oct/2020 19:38:20] "GET / HTTP/1.1" 200 -
+127.0.0.1 - - [05/Oct/2020 19:38:20] "GET / HTTP/1.1" 200 -
+127.0.0.1 - - [05/Oct/2020 19:38:20] "GET / HTTP/1.1" 200 -
+127.0.0.1 - - [05/Oct/2020 19:38:21] "GET / HTTP/1.1" 200 -
+127.0.0.1 - - [05/Oct/2020 19:38:21] "GET / HTTP/1.1" 200 -
+127.0.0.1 - - [05/Oct/2020 19:38:21] "GET / HTTP/1.1" 200 -
+127.0.0.1 - - [05/Oct/2020 19:38:21] "GET / HTTP/1.1" 200 -
+127.0.0.1 - - [05/Oct/2020 19:38:21] "GET / HTTP/1.1" 200 -
+127.0.0.1 - - [05/Oct/2020 19:38:22] "GET / HTTP/1.1" 200 -
+
---
+++ 2.WebDevelopment/demo/init.sh
@@ -0,0 +1,19 @@
+buiding HTTP REST APIs:
+-------------------------------->
+Flask
+Bottle
+Pyramid
+
+Full stack web application which touches both client and server:
+-------------------------------------------------------------------------------->
+Django
+TurboGears
+web2py
+
+
+full stack self contained web applications:
+--------------------------------------------->
+plone
+django-cms
+Mezzanine
+
